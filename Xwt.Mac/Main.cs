10cbaeb03bebc4753dfeb16fbeedfe6270439c2d
Dawid Borycki <dborycki@umk.pl>
2018-06-25 15:37:44 +0200

    Dawid Borycki
    Xwt.Mac

---
+++ Xwt.Mac/Main.cs
@@ -0,0 +1,12 @@
+﻿using Xwt.Common;
+
+namespace Xwt.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            XwtApp.Run(ToolkitType.XamMac);
+        }
+    }
+}
